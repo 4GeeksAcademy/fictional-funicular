ed915714ab37f4c7a87b399ce98cfdcd1b9650c5
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-03 20:20:48 +0000

    funciona

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 // function alerta() {
-    // let alert = document.getElementById('alerta').innerHTML = '<div class="alert alert-danger" role="alert"> <p class="float-start m-0">alertaaaaaaaaaaaaaaa</p> </div>';
-    // form.appendChild(alert);
+// let alert = document.getElementById('alerta').innerHTML = '<div class="alert alert-danger" role="alert"> <p class="float-start m-0">alertaaaaaaaaaaaaaaa</p> </div>';
+// form.appendChild(alert);
 // }
+
 
 function alertInput() {
     let form = document.getElementById('form');
     let alert = document.getElementById('alert')
+    // let alert2 = document.getElementById('alert2')
     for (let i = 0; i < form.length; i++) {
         if (form[i].value == '') {
             form[i].style.background = '#fcc';
             alert.removeAttribute('hidden');
+            event.preventDefault(); // <<<<<===== ESTO SE MARCA TACHADO, PERO SI LO QUITO SE ROMPE EL CODIGO, PQ? NO SÉ 
             //  alerta();
-        }
-    }     
+        // }if (form[i].value !== 'send' && form[i].value !== 'cancel') {
+        // alert2.removeAttribute('hidden');
+    }
+    }
 }
 
-// document.body.style.background = 'yellow'
+// document.body.style.background = 'red'
 // form[1].style.background = 'red'
 
 
+// form.validar(function(event){
+    
+// })
