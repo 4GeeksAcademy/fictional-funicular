a2278e5d4f0eeea09f5726c01a37149a92229dcb
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-03 04:42:46 +0000

    hidden alert

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+// function alerta() {
+    // let alert = document.getElementById('alerta').innerHTML = '<div class="alert alert-danger" role="alert"> <p class="float-start m-0">alertaaaaaaaaaaaaaaa</p> </div>';
+    // form.appendChild(alert);
+// }
+
 function alertInput() {
-let form = document.getElementById('form');
-for (let i = 0; i < form.length; i++) {
-    if (form[i].value == '') {
-        form[i].style.background = '#fcc'
-    }
-}
+    let form = document.getElementById('form');
+    let alert = document.getElementById('alert')
+    for (let i = 0; i < form.length; i++) {
+        if (form[i].value == '') {
+            form[i].style.background = '#fcc';
+            alert.removeAttribute('hidden');
+            //  alerta();
+        }
+    }     
 }
 
 // document.body.style.background = 'yellow'
 // form[1].style.background = 'red'
+
 
