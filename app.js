a7c42d456d6a9e4f38e8b97b4cfb1685e9a97128
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-04 15:23:09 +0000

    comento alertGreen

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-// function alerta() { //Intento de mostrar alerta
-// let alert = document.getElementById('alerta').innerHTML = '<div class="alert alert-danger" role="alert"> <p class="float-start m-0">alertaaaaaaaaaaaaaaa</p> </div>';
-// form.appendChild(alert);
-// }
-
 function validCredit() {
-    let credit = document.getElementById('credit');
+    let credit = document.querySelectorAll('input.option')
     let alert = document.getElementById('alert');
     for (let cards = 0; cards < credit.length; cards++) {
-        if (credit[cards].value == '') {
+        if (credit[cards].checked) {
             alert.removeAttribute('hidden');
             console.log('Falta el credit card');
-        } else {
-            continue
         }
     }
 }
+
+// function alertGreen() {
+//     let form = document.getElementById('form');
+//     let alert2 = document.getElementById('alert2');
+//     stopForm()
+//     for (let i = 0; i < form.length; i++) {
+//         if (form[i].values !== '' && form[i].value !== 'send' && form[i].value !== 'cancel' && form[i].value !== 'masterCard') {
+//             alert2.removeAttribute('hidden');
+//             console.log(form[i] + form[i].value);
+//         }
+//     }
+// }
 
 function stopForm() {
     form.addEventListener('click', function (event) {
@@ -25,25 +30,12 @@ function stopForm() {
 function alertInput() {
     let form = document.getElementById('form');
     let alert = document.getElementById('alert');
-    let alert2 = document.getElementById('alert2');
     stopForm()
-    alertGreen()
+    // alertGreen()
     for (let i = 0; i < form.length; i++) {
         if (form[i].value == '') {
-            form[i].style.background = '#fcc';
+            form[i].style.border = '1px solid #f66';
             alert.removeAttribute('hidden');
-        }
-    }
-}
-
-function alertGreen() {
-    let form = document.getElementById('form');
-    let alert2 = document.getElementById('alert2');
-    stopForm()
-    for (let i = 0; i < form.length; i++) {
-        if (form[i].value !== '' && form[i].value !== 'send' && form[i].value !== 'cancel' && form[i].value !== 'masterCard') {
-            alert2.removeAttribute('hidden');
-            console.log(form[i] + form[i].value);
         }
     }
 }
