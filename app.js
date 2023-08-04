3d79afa889564312fc6376be35f02bf9fba18572
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-04 15:55:19 +0000

    check credit card

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ function validCredit() {
     let credit = document.querySelectorAll('input.option')
     let alert = document.getElementById('alert');
     for (let cards = 0; cards < credit.length; cards++) {
-        if (credit[cards].checked) {
+        if (!credit[cards].checked) {
             alert.removeAttribute('hidden');
-            console.log('Falta el credit card');
         }
     }
 }
 
-// function alertGreen() {
-//     let form = document.getElementById('form');
-//     let alert2 = document.getElementById('alert2');
-//     stopForm()
-//     for (let i = 0; i < form.length; i++) {
-//         if (form[i].values !== '' && form[i].value !== 'send' && form[i].value !== 'cancel' && form[i].value !== 'masterCard') {
-//             alert2.removeAttribute('hidden');
-//             console.log(form[i] + form[i].value);
-//         }
-//     }
-// }
+function greenAlert() {
+    let form = document.getElementById('form');
+    let alert2 = document.getElementById('alert2');
+    stopForm()
+    for (let i = 0; i < form.length; i++) {
+        if (form[i].values !== '' && form[i].value !== 'send' && form[i].value !== 'cancel' && form[i].value !== 'masterCard') {
+            alert2.removeAttribute('hidden');
+        }
+    }
+}
 
 function stopForm() {
     form.addEventListener('click', function (event) {
@@ -31,11 +29,11 @@ function alertInput() {
     let form = document.getElementById('form');
     let alert = document.getElementById('alert');
     stopForm()
-    // alertGreen()
     for (let i = 0; i < form.length; i++) {
         if (form[i].value == '') {
             form[i].style.border = '1px solid #f66';
             alert.removeAttribute('hidden');
         }
     }
+    validCredit()
 }
